304fb974301d686d1a27c4474123dd2ab3128bad
range3 <white.line.oar@gmail.com>
2019-04-04 14:05:17 +0900

    Add rustup.bash-completion

--- scripts/post-link/300_rust.sh
+++ scripts/post-link/300_rust.sh
@@ -1,4 +1,6 @@
 if [ ! -d $HOME/.cargo ]; then
   curl https://sh.rustup.rs -sSf \
     | bash -s -- -y --no-modify-path
+  ${HOME}/.cargo/bin/rustup completions bash \
+    > ${HOME}/.bash_completion.d/rustup.bash-completion
 fi
