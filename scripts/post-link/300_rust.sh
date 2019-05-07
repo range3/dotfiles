472ad2d26790f62198067a3399878d2d742e0ecc
range3 <white.line.oar@gmail.com>
2019-05-07 23:47:58 +0900

    Add a bash completion for Cargo

--- scripts/post-link/300_rust.sh
+++ scripts/post-link/300_rust.sh
@@ -1,6 +1,9 @@
 if [ ! -d $HOME/.cargo ]; then
   curl https://sh.rustup.rs -sSf \
     | bash -s -- -y --no-modify-path
-  ${HOME}/.cargo/bin/rustup completions bash \
-    > ${HOME}/.bash_completion.d/rustup.bash-completion
 fi
+
+${HOME}/.cargo/bin/rustup completions bash rustup \
+  > ${HOME}/.bash_completion.d/rustup.bash-completion
+${HOME}/.cargo/bin/rustup completions bash cargo \
+  > ${HOME}/.bash_completion.d/cargo.bash-completion
