5b6502528b042152a8426d7c36baccbca2ce9e1f
range3 <white.line.oar@gmail.com>
2019-05-17 16:07:06 +0900

    update linuxbrew envs

--- bash_profile.d/100_linuxbrew.sh
+++ bash_profile.d/100_linuxbrew.sh
@@ -1,8 +1,4 @@
 # Linuxbrew
 if [ -d "$HOME/.linuxbrew" ]; then
-  export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-  export MANPATH="$(brew --prefix)/share/man:$MANPATH"
-  export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
-  export XDG_DATA_DIRS="$(brew --prefix)/share:$XDG_DATA_DIRS"
+  eval "$($HOME/.linuxbrew/bin/brew shellenv)"
 fi
-
