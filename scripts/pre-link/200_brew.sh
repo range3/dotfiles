145fd74faa18ac8a18b7329493b2e16635c8cbfe
range3 <white.line.oar@gmail.com>
2019-05-24 11:18:48 +0900

    Update Linuxbrew installation

--- scripts/pre-link/200_brew.sh
+++ scripts/pre-link/200_brew.sh
@@ -1,3 +1,6 @@
 if [ ! -d $HOME/.linuxbrew ]; then
-  git clone https://github.com/Linuxbrew/brew.git $HOME/.linuxbrew
+  git clone https://github.com/Homebrew/brew $HOME/.linuxbrew/Homebrew
+  mkdir ~/.linuxbrew/bin
+  ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+  eval $($HOME/.linuxbrew/bin/brew shellenv)
 fi
