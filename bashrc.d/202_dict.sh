679bbd78b94f4fc5665f413e5739ff4e7c96f0d4
range3 <white.line.oar@gmail.com>
2019-07-29 01:40:17 +0900

    Update dict
    show information if the dictionary file does not exist

--- bashrc.d/202_dict.sh
+++ bashrc.d/202_dict.sh
@@ -6,6 +6,10 @@ DICT_PATH="${HOME}/.gene.txt"
 function ejdict() {
   if [ -f "${DICT_PATH}" ]; then
     grep "$*" "${DICT_PATH}" -E -A 1 -wi --color=always | less -R -FX
+  else
+    echo "The GENE95 dictionary file does not exist." >&2
+    echo "Download it to ${DICT_PATH} yourself." >&2
+    echo "http://www.namazu.org/~tsuchiya/sdic/data/gene.html" >&2
   fi
 }
 
@@ -14,6 +18,10 @@ alias dict=ejdict
 function jedict() {
   if [ -f "${DICT_PATH}" ]; then
     grep "$*" "${DICT_PATH}" -E -B 1 -wi --color=always | less -R -FX
+  else
+    echo "The GENE95 dictionary file does not exist." >&2
+    echo "Download it to ${DICT_PATH} yourself." >&2
+    echo "http://www.namazu.org/~tsuchiya/sdic/data/gene.html" >&2
   fi
 }
 
