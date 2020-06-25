454fd09b5cb5f057fd010b93037de19ebf0668d1
range3 <white.line.oar@gmail.com>
2020-06-25 17:39:53 +0900

    Update docker-tags: sort ouput tags

--- bashrc.d/203_docker-tags.sh
+++ bashrc.d/203_docker-tags.sh
@@ -1,3 +1,3 @@
 function docker-tags {
-  curl -s https://registry.hub.docker.com/v1/repositories/$1/tags | jq -r '.[].name'
+  curl -s https://registry.hub.docker.com/v1/repositories/$1/tags | jq -r '.[].name' | sort -n
 }
