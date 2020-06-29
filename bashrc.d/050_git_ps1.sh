7b47274e1f98267663c65b229f6db67ce17d0314
range3 <white.line.oar@gmail.com>
2020-06-30 00:02:27 +0900

    Update PS1

--- bashrc.d/050_git_ps1.sh
+++ bashrc.d/050_git_ps1.sh
@@ -1,3 +1,3 @@
 if command -v __git_ps1 1>/dev/null 2>&1; then
-  PS1='\u@\h:\w$(__git_ps1)\\$ '
+  PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1)\$ '
 fi
