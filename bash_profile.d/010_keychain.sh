470193900d1a7686e1dcd55461ff3a3b0ac51ab4
range3 <white.line.oar@gmail.com>
2021-07-30 16:59:55 +0900

    Update keychain

--- bash_profile.d/010_keychain.sh
+++ bash_profile.d/010_keychain.sh
@@ -3,8 +3,7 @@ if command -v keychain 1>/dev/null 2>&1; then
   keychain \
     --quiet \
     --ignore-missing \
-    $HOME/.ssh/id_rsa \
-    $HOME/.ssh/id_dsa
+    $(find $HOME/.ssh -name 'id_*' -and -not -name '*.pub')
 
   eval "$(keychain --quiet --eval)"
 fi
